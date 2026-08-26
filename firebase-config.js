d90d4cb047326613eb5aae3cf1ad2a04bf6deb67
Firilare <132409311+Firilare@users.noreply.github.com>
2026-08-27 01:52:43 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.__FROSTBORNE_FIREBASE_CONFIG__ = {
-  apiKey: 'AIzaSyAHBvCMazcHJvajUC5MG4lyM5vGycMCPAE',
-  authDomain: 'frostborne-1ca0b.firebaseapp.com',
-  databaseURL: 'https://frostborne-1ca0b-default-rtdb.firebaseio.com',
-  projectId: 'frostborne-1ca0b',
-  storageBucket: 'frostborne-1ca0b.firebasestorage.app',
-  messagingSenderId: '1097451279446',
-  appId: '1:1097451279446:web:e31985e3b1d13b168a17f7'
-};
